2eb5d06a926600fefdd3174e42ae3b267e52f3a9
frameless Media <office@frameless.at>
2026-04-04 16:45:01 +0200

    added href to ModuleInfo()

--- StripePlCustomerPortal.module.php
+++ StripePlCustomerPortal.module.php
@@ -29,6 +29,7 @@ class StripePlCustomerPortal extends WireData implements Module {
       'version'  => '0.1.7',
       'summary'  => 'Customer overview at /account using a dedicated template (spl_account).',
       'author'   => 'frameless Media',
+      'href'     => 'https://github.com/frameless-at/StripePlCustomerPortal',
       'autoload' => true,
       'singular' => true,
       'requires' => ['ProcessWire>=3.0.210', 'StripePaymentLinks'],
